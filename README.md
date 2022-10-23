cf0e8c4f7a206d315abf16026892aed5be89ed99
Subramanya1702 <keshavas@oregonstate.edu>
2022-10-22 17:23:52 -0700

    Update README document

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# CrackStation
+# What is CrackStation?
 
-A description of this package.
+###CrackStation is a library that is capable of cracking a hashed password. 
+
+###The library can crack passwords upto three characters in length which are the comibinations of characters "A-Z", "a-z", "0-9", "?" and "!".
+
+
+#How to run CrackStation?
+
+###CrackStation depends on a json lookup file to crack passwords. So, make sure the **DictionaryPasswords.json** file is present under the **/Sources** directory. If not, follow the steps below.
+
+###Go to the **/HashGenerator** directory and execute the **hashgenerator** jar file.
+
+###`java -jar JAR_NAME.jar`
+
+###After executing the jar file, the DictionaryPasswords.json file should have been created under the Sources directory.
+
+###Add the package CrackStation to the desired application and import the dependency. 
+###CrackStation has exposed a function `crack(password: String)` that takes in the hashed password and gives back the cracked/actual password.
+
--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -1,6 +1,35 @@
-public struct CrackStation {
-    public private(set) var text = "Hello, World!"
+import Foundation
+
+public class CrackStation {
+    
+    var hashDict: Dictionary<String, String> = [:]
 
     public init() {
+    }
+    
+    public func crack(password: String) throws -> String? {
+        try loadDictionaryFromDisk()
+        
+        if hashDict.isEmpty {
+            print("The hash file is empty. Please run the java program to generate the hash file")
+            return nil
+        }
+        
+        if hashDict[password] != nil {
+            return hashDict[password]
+        } else {
+            return nil
+        }
+    }
+    
+    private func loadDictionaryFromDisk() throws {
+        guard let path = Bundle.module.url(forResource: "DictionaryPasswords", withExtension: "json") else { return }
+
+        let data = try Data(contentsOf: path)
+        let jsonResult = try JSONSerialization.jsonObject(with: data)
+
+        if let dict: Dictionary = jsonResult as? Dictionary<String, String> {
+            hashDict = dict
+        }
     }
 }
