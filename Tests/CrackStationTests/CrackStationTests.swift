184c75292632b4b2173d271d4465be1519467bc4
Subramanya1702 <keshavas@oregonstate.edu>
2022-10-22 17:23:52 -0700

    Initial commit: Crack station

--- Tests/CrackStationTests/CrackStationTests.swift
+++ Tests/CrackStationTests/CrackStationTests.swift
@@ -2,10 +2,11 @@ import XCTest
 @testable import CrackStation
 
 final class CrackStationTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CrackStation().text, "Hello, World!")
+    
+    func testCrackStation() throws {
+        let crackStation = CrackStation()
+        let actualPassword = try crackStation.crack(password: "12abf551138756adc2a88edc23cb77b1832b7ab8c")
+        XCTAssertNotNil(actualPassword)
+        XCTAssert("aac".elementsEqual(actualPassword!))
     }
 }
